1e6b775ae283b5a51e036f7044c9201e97204c5b
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-26 11:17:31 -0600

    Add postUrls post function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,13 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrls = ({long_url, title}) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({long_url: long_url, title: title})
+  })
+  .then(res => res.json())
+  .then (data => console.log(data))
+}
